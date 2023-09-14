69d5d7461f047ebd711ca9bc705cffc829a96d6f
Dwi <wdwihartarto@gmail.com>
2023-09-14 09:40:01 +0700

    fix untrack gitignore

--- lib/secure.dart
+++
@@ -1,6 +0,0 @@
-const hostRabbitMq = '202.91.9.14';
-const virtualHostRabbitMq = 'request_otp';
-const passwordRabbitMq = '8FNOV7zpmVCr';
-const usernameRabbitMq = 'amikomone';
-const portRabbitMq = 5672;
-const queueName = 'OTP_NIM';
